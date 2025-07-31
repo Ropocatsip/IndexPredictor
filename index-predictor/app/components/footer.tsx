95f44d4119b425efe3419c28722d605ad59d8c47
Nudthaya Khemcharoen <nudthaya@sec.or.th>
2025-07-31 21:12:19 +0700

    สร้าง header, footer

---
+++ index-predictor/app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() 
+{
+    return (<>
+        <footer className="mt-5">
+            <hr />
+            <p className="text-muted">National Electronics and Computer Technology Center x Chulalongkorn Universityc</p>
+        </footer>
+    </>);
+}
---
+++ index-predictor/app/components/header.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Header() 
+{
+    return (<>
+        <header className="py-3 bg-light text-center">
+            IndexPredictor
+        </header>
+    </>);
+}
